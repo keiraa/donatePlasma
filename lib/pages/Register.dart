8287a3d278ebf58891d8a047e1f4b193e3a4f6b5
akhilpasumarthi <pasumarthiakhil66@gmail.com>
2021-05-09 22:33:13 +0530

    Fixed Bugs

--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -33,7 +33,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomPadding: true,
+        resizeToAvoidBottomInset: true,
         body: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
